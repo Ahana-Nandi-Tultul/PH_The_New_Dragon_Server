41c0b5bce23a94c1ed6e9b3980e194f7f0b01fc2
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-11 18:24:47 +0600

    End Point for news and category

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 app.use(cors())
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News Dragon is comming.')
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) =>{
     res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, () => {
